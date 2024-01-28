a59aa495d60458d85e6795b4577dfcdf1a33a136
Kaiyuan Qian <116858989+kaiyuanqian@users.noreply.github.com>
2024-01-28 15:21:44 +1100

    Initial Commit

---
+++ Wordcross/WordcrossApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordcrossApp.swift
+//  Wordcross
+//
+//  Created by Kaiyuan Qian on 28/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordcrossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
