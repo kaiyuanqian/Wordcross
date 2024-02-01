480157bdf297c79f5711ebb3deb9f9382ffdc8cd
Kaiyuan Qian <116858989+kaiyuanqian@users.noreply.github.com>
2024-02-01 17:42:24 +1100

    made score optional

--- Wordcross/Tile.swift
+++ Wordcross/Tile.swift
@@ -10,24 +10,41 @@ import SwiftUI
 
 struct Tile {
     var letter: String
-    var score: Int
+    var score: Int?
     
     func displayTile() -> AnyView {
-        return AnyView(HStack {
-            Text(letter)
-                .font(.system(size: 60))
-            
-            Text(String(score))
-                .font(.system(size: 22))
-                .offset(y: 15)
+        
+        if score != nil {
+            return AnyView(HStack {
+                Text(letter)
+                    .font(.system(size: 60))
+                
+                Text(String(score!))
+                    .font(.system(size: 22))
+                    .offset(y: 15)
+            }
+                
+                .foregroundColor(.black)
+                .bold()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .aspectRatio(1, contentMode: .fit)
+                .background(Color.white)
+                .border(.black, width: 1)
+            )
+        } else {
+            return AnyView(HStack {
+                Text(letter)
+                    .font(.system(size: 60))
+            }
+                
+                .foregroundColor(.black)
+                .bold()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .aspectRatio(1, contentMode: .fit)
+                .background(Color.white)
+                .border(.black, width: 1)
+            )
         }
-            
-            .foregroundColor(.black)
-            .bold()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .aspectRatio(1, contentMode: .fit)
-            .background(Color.white)
-            .border(.black, width: 1)
-        )
+        
     }
 }
