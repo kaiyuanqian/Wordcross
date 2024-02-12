529d0b4015ef8d061d4cf48658669a86f1db72a5
Kaiyuan Qian <116858989+kaiyuanqian@users.noreply.github.com>
2024-02-12 18:18:32 +1100

    implement function that calculates the num of characters to the left of a new character

--- Wordcross/GameState.swift
+++ Wordcross/GameState.swift
@@ -15,6 +15,8 @@ class GameState: ObservableObject {
     let alphabet: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     let bagSize = 25
     let tileScores = ["A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8, "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10]
+    let maxWordLen = 5
+    let minWordLen = 3
     
     var numPlaced = 0 // number of letters that have been placed
     
@@ -35,7 +37,7 @@ class GameState: ObservableObject {
         }
     }
     
-    // places a letter on the board
+    // places a letter on the board and checks whether any words were formed
     func placeLetter(_ row: Int, _ column: Int) {
         // is the cell empty?
         if (board[row][column].tile.letter != "") {
@@ -47,6 +49,25 @@ class GameState: ObservableObject {
         
         // move on to next letter
         numPlaced += 1
+        
+        leftLength(row, column)
+    }
+    
+    // calculates the number of letters directly before the letter that just got placed
+    func leftLength(_ row: Int, _ column: Int) -> Int {
+        var currCol = column // we initially start w the new letter which was placed in this column
+        var leftLen = 0
+        var tile: Tile = board[row][currCol].tile
+        
+        while tile.letter != "" && currCol > 0 {
+            tile = board[row][currCol - 1].tile
+            currCol -= 1
+            if tile.letter != "" {
+                leftLen += 1
+            }
+        }
+
+        return leftLen
     }
     
     // reads the words dictionary
@@ -68,9 +89,10 @@ class GameState: ObservableObject {
     // remove every word that isn't between 3 and 5 characters in length
     func retainSuitableWords() {
         Swift.print(allWords.count)
-        allWords.removeAll { $0.count > 5}
-        allWords.removeAll { $0.count < 3}
+        allWords.removeAll { $0.count > maxWordLen}
+        allWords.removeAll { $0.count < minWordLen}
         Swift.print(allWords.count)
+        Swift.print(allWords[0])
     }
     
     // creates an empty board
